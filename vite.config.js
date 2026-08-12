2382daab63b5ebd54f8a24077da757409fa5dfd1
Perdana Kurniawan <perdanakurniawan25@gmail.com>
2026-08-12 19:02:45 +0700

    feat: successfully include sitemap.xml in production build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react()],
-
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/sitemap.xml',
+          dest: '' 
+        }
+      ]
+    })
+  ],
   build: {
     cssMinify: 'esbuild',
+    emptyOutDir: true,
   },
 })
